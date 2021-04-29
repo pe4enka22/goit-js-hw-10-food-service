a23cdeb38ac82849a99e71dfcb623ca0b8de0990
pe4enka22 <missis.bug@gmail.com>
2021-04-29 23:07:02 +0200

    attempt to apply themes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ window.onload = () => {
     const cards = document.querySelector('.js-menu');
     cards.innerHTML = template(testData);
 }
+
+
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const switcher = document.querySelector('.theme-switch__toggle');
+switcher.addEventListener('change', changeTheme);
+function changeTheme(){
+    if (switcher.checked) {
+        body.classList.add(`${Theme.DARK}`);
+        body.classList.remove(`${Theme.LIGHT}`);
+    } else {
+        body.classList.remove(`${Theme.DARK}`);
+        body.classList.add(`${Theme.LIGHT}`);
+    }
+}
